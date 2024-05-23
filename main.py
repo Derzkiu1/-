aa2558060b3013c25ab8a42d0eeca77b4690d5d7
Derzkiu1 <153546822+Derzkiu1@users.noreply.github.com>
2024-05-23 12:23:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.context import FSMContext
 from aiohttp import web
 from aiogram.filters.command import Command
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo,InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = Bot(token='7028494517:AAEiIPjbRdn7EY-IupFwIF9pEBegJaUogDI')
 dp = Dispatcher()
 
 USERS_FILE = 'users.json'
-TOP_HTML_FILE = 'top.html'
-
-
 
 def load_users():
     try:
@@ -27,14 +24,6 @@ def save_users(users):
     with open(USERS_FILE, 'w', encoding='utf-8') as file:
         json.dump(users, file, ensure_ascii=False, indent=4)
 
-def update_top_html(users):
-    sorted_users = sorted(users, key=lambda x: x['balance'], reverse=True)[:10]
-    with open(TOP_HTML_FILE, 'w', encoding='utf-8') as file:
-        file.write('<ul>\n')
-        for user in sorted_users:
-            file.write(f'<li>{user["username"]}: Баланс {user["balance"]}, Население {user["population"]}</li>\n')
-        file.write('</ul>\n')
-
 async def save_user_info(user_id: int, username: str, first_name: str, league: str, balance: int, population: int):
     users = load_users()
     user_info = {
@@ -46,22 +35,36 @@ async def save_user_info(user_id: int, username: str, first_name: str, league: s
         "population": population,
         "totalProfit": 0,
         "items": [
-            {"name": "Помощь бедным", "price": 100, "priceIncrement": 25, "profit": 25, "profitIncrement": 2.5,
-             "population": 10, "populationIncrement": 1, "level": 0}
+            {
+                "name": "Помощь бедным",
+                "price": 100,
+                "priceIncrement": 25,
+                "profit": 25,
+                "profitIncrement": 2.5,
+                "population": 10,
+                "populationIncrement": 1,
+                "level": 0
+            }
         ]
     }
 
-    # Проверяем, существует ли пользователь в базе данных
     existing_user = next((user for user in users if user['user_id'] == user_id), None)
     if existing_user:
-        existing_user.update(user_info)
+        # Обновляем информацию о существующем пользователе, сохраняя уровни товаров
+        existing_user.update({
+            "username": username,
+            "first_name": first_name,
+            "league": league,
+            "balance": balance,
+            "population": population
+        })
     else:
-        user_info['balance'] = 1000
-        user_info['population'] = 250
+        # Новый пользователь получает начальные значения
+        user_info['balance'] = 500
+        user_info['population'] = 150
         users.append(user_info)
 
     save_users(users)
-    update_top_html(users)
 
 @dp.message(Command('start'))
 async def start(message: types.Message):
@@ -74,8 +77,13 @@ async def start(message: types.Message):
 
     await save_user_info(user_id, username, first_name, league, balance, population)
 
-    item1 = KeyboardButton(text="Open", web_app=WebAppInfo(url='https://derzkiu1.github.io/-/'))
-    keyboard = ReplyKeyboardMarkup(keyboard=[[item1]], resize_keyboard=True)
+    # Создаем инлайн-кнопку с WebAppInfo
+    web_app_button = InlineKeyboardButton(
+        text="Open",
+        web_app=WebAppInfo(url='https://derzkiu1.github.io/-/')
+    )
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[web_app_button]])
+
     await bot.send_message(message.from_user.id, 'Welcome', reply_markup=keyboard)
 
 async def main():
